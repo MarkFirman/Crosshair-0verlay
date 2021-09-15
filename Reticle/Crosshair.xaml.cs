b7bab5bc3f9157627b0e853514131ed1fd5f2149
MarkFirman <mark.firman@me.com>
2021-09-15 19:26:06 +0100

    First Release
    First (and probably only) release.

---
+++ Reticle/Crosshair.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Reticle
+{
+    /// <summary>
+    /// Interaction logic for Crosshair.xaml
+    /// </summary>
+    public partial class Crosshair : Window
+    {
+
+        /// <summary>
+        /// Holds reference to the settings window
+        /// </summary>
+        private MainWindow settingsWindow;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="crosshairSettingsWindow"></param>
+        public Crosshair(MainWindow crosshairSettingsWindow)
+        {
+            InitializeComponent();
+
+            // Set reference to the settings window
+            this.settingsWindow = crosshairSettingsWindow;
+
+            // Set self window to be center of the screen
+            // Startup is in the middle of the screen, but that does not always work, so we should get the screen settings
+            // including height and width and set the to the center manually
+
+            // Set the cross hair image
+            CrossHairImageOverlay.Source = settingsWindow.ReticleImage;
+
+            // Set the window focus
+            // The topmost setting cannot be used until the focus of the window has been set
+            this.Focus();
+            
+        }
+
+        /// <summary>
+        /// Force crosshair to be topmost when always on top is checked
+        /// This is required when the game forces itself fullscreen
+        /// Occurs when the window is no longer the frame of reference
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+            if (this.settingsWindow.AlwaysOnTopCheckbox.IsChecked == true)
+            {
+                // Create a new action to continually set the window to topmost
+                // Intentionally do not await the result
+                // Some games may call themselves topmost over and over, therefore we also need to repeat to prevent
+                // third party apps from taking the top
+                // This can cause flickering, but that can be dampened using waits and retries
+                App.Current.Dispatcher.BeginInvoke(new Action(async () => await RetrySetTopMost()));
+            }
+        }
+
+        /// <summary>
+        /// This will force the window to be the topmost
+        /// The window must get focus with a mouse click to work, though.
+        /// Best bet is to simulate a mouse click on the window first
+        /// </summary>
+        private const int RetrySetTopMostDelay = 1000;
+        private const int RetrySetTopMostMax = 9999999;
+        private static async Task RetrySetTopMost()
+        {
+            try
+            {
+                for (int i = 0; i < RetrySetTopMostMax; i++)
+                {
+                    await Task.Delay(RetrySetTopMostDelay);
+
+                    foreach (Window w in App.Current.Windows)
+                    {
+                        if (w.Title == "Crosshair")
+                        {
+
+                            w.Topmost = false;
+                            w.Topmost = true;
+
+                            break;
+                        }
+                    }
+
+
+                }
+
+                // Intentionally do not await the result
+                App.Current.Dispatcher.BeginInvoke(new Action(async () => await RetrySetTopMost()));
+
+            } catch(Exception ex)
+            {
+                MessageBox.Show("An error occured whilst attempting to overlay the crosshair");
+            }
+
+        }
+
+
+        /// <summary>
+        /// Hides / Shows the cross hair window when the hotkey is pressed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.Key == Key.OemPeriod)
+                {
+                    if (this.Visibility == Visibility.Visible)
+                    {
+                        this.Visibility = Visibility.Collapsed;
+                    }
+                    else
+                    {
+                        this.Visibility = Visibility.Visible;
+                    }
+                }
+
+                this.Focus();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occured whilst attempting to use the hotkey");
+            }
+        }
+
+    }
+}
---
+++ Reticle/CrosshairSettings.xaml.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+
+namespace Reticle
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        /// <summary>
+        /// Holds the chosen reticle image
+        /// </summary>
+        public BitmapImage ReticleImage;
+
+        /// <summary>
+        /// Holds the selected preset
+        /// </summary>
+        private int ChosenPreset = 0;
+
+        /// <summary>
+        /// Holds reference to the crosshair window
+        /// </summary>
+        private Crosshair crosshairWindow;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>
+        /// Starts the reticle overlay
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StartRecticleButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                switch (StartCrosshairButton.Content)
+                {
+
+                    case "STOP CROSSHAIR OVERLAY":
+
+                        // Change start crosshair overlay button text to reflect the fact that overlay has started
+                        StartCrosshairButton.Content = "START CROSSHAIR OVERLAY";
+
+                        // Close the crosshair window
+                        crosshairWindow.Close();
+
+                        break;
+
+                    case "START CROSSHAIR OVERLAY":
+
+                        // Ensure a crosshair has been selected
+                        if (ReticleImage == null)
+                        {
+                            MessageBox.Show("No crosshair selected!");
+                            return;
+                        }
+
+                        // Change start crosshair overlay button text to reflect the fact that overlay has started
+                        StartCrosshairButton.Content = "STOP CROSSHAIR OVERLAY";
+
+                        // Load the crosshair window
+                        crosshairWindow = new Crosshair(this);
+                        crosshairWindow.Show();
+
+                        break;
+
+                }
+
+            } catch(Exception ex)
+            {
+                MessageBox.Show("An error occured starting the crosshair overlay");
+            }
+
+        }
+
+        /// <summary>
+        /// Choose new reticle image button click event
+        /// Occurs when the "Choose Image..." button is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ChooseReticleImage_Button(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                // New instance of the open file dialog from the Win32 API
+                OpenFileDialog dialog = new OpenFileDialog();
+
+                // Set the dialog filters so only PNG and GIFs can be opened
+                // We only use these filetypes as they support transparency
+                dialog.Filter = "Transparent Image Files (*.png;*.gif)|*.png;*.gif";
+
+                // Set the dialog initial directory to the desktop
+                dialog.InitialDirectory = String.Format(@"C:\Users\{0}\Desktop\", Environment.UserName);
+
+                // Disable multiselect
+                dialog.Multiselect = false;
+
+                // Open the dialog for the user to choose image
+                if (dialog.ShowDialog() == true)
+                {
+                    // Load the selected image into memory as a bitmap
+                    ReticleImage = new BitmapImage();
+                    ReticleImage.BeginInit();
+                    ReticleImage.UriSource = new Uri(dialog.FileName);
+                    ReticleImage.EndInit();
+
+                    // Set the chosen image to the preview window
+                    ReticleImagePreview.Source = ReticleImage;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occured when choosing the crosshair image");
+            }
+        }
+
+        /// <summary>
+        /// Event is called when a preset is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UsePresetButton_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                // Get the selected preset tag ID
+                string selectedPreset = ((Button)sender).Tag.ToString();
+
+                // Disable the previously selected preset (if exists)
+                if (ChosenPreset != 0)
+                {
+                    // Remote the existing border change
+                    // Find the button that corrosponds with the chosen preset value
+                    foreach (object child in LogicalTreeHelper.GetChildren(PresetPanel))
+                    {
+                        Border b = (Border)child;
+                        if (b.Tag.ToString() == ChosenPreset.ToString())
+                        {
+                            b.BorderBrush = (SolidColorBrush)new BrushConverter().ConvertFromString("Gray");
+                            break;
+                        }
+
+                    }
+                }
+
+                // Set the border of the chosen preset to show that it has been selected
+                ChosenPreset = int.Parse(selectedPreset);
+
+                // Set the border of the chosen preset to highlight that it has been selected
+                foreach (object child in LogicalTreeHelper.GetChildren(PresetPanel))
+                {
+                    Border b = (Border)child;
+                    if (b.Tag.ToString() == selectedPreset.ToString())
+                    {
+                        b.BorderBrush = (SolidColorBrush)new BrushConverter().ConvertFromString("Green");
+                        break;
+                    }
+
+                }
+
+                // Apply the cross hair to the preview image pane
+                ReticleImage = new BitmapImage();
+                ReticleImage.BeginInit();
+                ReticleImage.UriSource = new Uri("pack://application:,,,/Crosshairs/" + selectedPreset + ".png");
+                ReticleImage.EndInit();
+                ReticleImagePreview.Source = ReticleImage;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occured when attempting to use a crosshair preset");
+            }
+
+        }
+
+        /// <summary>
+        /// Occurs when the window is no longer the frame of reference
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Event occurs when the window is closed
+        /// Force closes the application
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown(0);
+        }
+    }
+}
